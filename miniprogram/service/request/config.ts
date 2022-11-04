29e2cc562ba119d344d3394a0731cab22a5ba243
simon <simonfisher227@gmail.com>
2022-11-04 16:49:17 +0800

    change api config

--- miniprogram/service/request/config.ts
+++ miniprogram/service/request/config.ts
@@ -1,4 +1,3 @@
-// export const BASE_URL = "https://ywt-music.vercel.app";
-export const BASE_URL = "http://codercba.com:9002";
+export const BASE_URL = "https://ywt-music.vercel.app";
 
 export const TIME_OUT = 5000;
